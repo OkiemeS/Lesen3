77090cf93972f44e34b5923c0da1be3e0100731e
Павел Семейко <pawel.semeiko@yandex.ru>
2021-09-24 11:33:32 +0300

    Добавьте файлы проекта.

---
+++ Kal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesen3
+{
+    class Kal
+    {
+        public int Kalculator(int odin, int dwa)
+        {
+            int result;
+            {
+                result = odin * dwa;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesen3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите умножаемое число");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите множитель");
+            string b = Console.ReadLine();
+            int c = Convert.ToInt32(a);
+            int d = Convert.ToInt32(b);
+            int result;
+            Kal kal = new Kal();
+            result = kal.Kalculator(c,d);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
